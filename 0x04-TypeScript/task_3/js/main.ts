59f9ddb0a0418c43a876d352067bd209830c03b7
vitroyfix <peterwangimwa@gmail.com>
2025-11-09 20:56:26 +0300

    Refactor employee management and implement CRUD operations
    Refactor employee management by removing interfaces and classes for Director and Teacher. Introduce CRUD operations for managing employee data.

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,56 +1,16 @@
-interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
+/// <reference path="./crud.d.ts" />
 
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud.js';
 
-class Director implements DirectorInterface {
-  workFromHome = () => 'Working from home';
-  getCoffeeBreak = () => 'Getting a coffee break';
-  workDirectorTasks = () => 'Getting to director tasks';
-}
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
 
-class Teacher implements TeacherInterface {
-  workFromHome = () => 'Cannot work from home';
-  getCoffeeBreak = () => 'Cannot have a break';
-  workTeacherTasks = () => 'Getting to work';
-}
+const newRowID: RowID = CRUD.insertRow(row);
 
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number') {
-    if (salary < 500) {
-      return new Teacher();
-    }
-  }
-  return new Director();
-}
+const updatedRow: RowElement = { ...row, age: 23 };
 
-export function isDirector(employee: Director | Teacher): employee is Director {
-  return (employee as Director).workDirectorTasks !== undefined;
-}
-
-function executeWork(employee: Director | Teacher): string {
-  if (isDirector(employee)) {
-    return employee.workDirectorTasks();
-  } else {
-    return employee.workTeacherTasks();
-  }
-}
-
-type Subjects = 'Math' | 'History';
-
-function teachClass(todayClass:Subjects): string {
-  if (todayClass === 'Math') {
-    return 'Teaching Math';
-  }
-  if (todayClass === 'History') {
-    return 'Teaching History';
-  }
-  return '';
-}
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
