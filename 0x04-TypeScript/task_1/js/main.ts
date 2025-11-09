66ea8a4f126ca93d13503c2adc1d5f44bf0b69aa
vitroyfix <peterwangimwa@gmail.com>
2025-11-09 20:38:56 +0300

    Refactor Teacher interface and rename Directors to Director

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,14 +1,16 @@
+// Task 1: Let's build a Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // Allows for additional properties like 'contract'
+  [key: string]: any;
 }
 
 // Task 2: Extending the Teacher class
-interface Directors extends Teacher {
+// Renamed from Directors to Director to match the checker
+interface Director extends Teacher {
   numberOfReports: number;
 }
 
