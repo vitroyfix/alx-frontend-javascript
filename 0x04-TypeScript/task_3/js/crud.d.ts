1c9111b6ea4ae6d13c7a247f570c6c2b5512bc5d
vitroyfix <peterwangimwa@gmail.com>
2025-11-09 20:56:51 +0300

    Change return type of insertRow to number

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,8 +1,9 @@
 import { RowID, RowElement } from './interface';
 
-// This declares the types for the file './crud.js'
 declare module './crud.js' {
-  export function insertRow(row: RowElement): RowID;
+  // Use 'export' because it's a module
+  // Use 'number' as the return type for insertRow
+  export function insertRow(row: RowElement): number;
   export function deleteRow(rowId: RowID): void;
   export function updateRow(rowId: RowID, row: RowElement): RowID;
 }
